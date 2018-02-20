db3f6e63b52a50bdc4f4e3ce13d6890726e34b54
Loïc <loic.gillioz@outlook.com>
2018-02-20 15:53:57 +0100

    corrected typo

--- stack.hpp
+++ stack.hpp
@@ -7,7 +7,7 @@ class Stack {
 public:
     Stack(std::size_t capacity): storage_(new T[capacity]), capacity_(capacity) {}
 	~Stack(){
-		delete storage;
+		delete storage_;
 	}
     inline std::size_t capacity() const {
         return capacity_;
