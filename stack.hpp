a9c5e70615a0d154b6934bdff65de97c0969fee9
Tristan <renontristan@hotmail.com>
2018-02-20 16:16:16 +0100

    Memory leaks

--- stack.hpp
+++ stack.hpp
@@ -7,7 +7,7 @@ template <typename T>
 class Stack {
 public:
 	Stack(std::size_t capacity): storage_(new T[capacity]), capacity_(capacity) {}
-	~Stack(){}
+	~Stack(){delete[] storage_;}
 
     inline std::size_t capacity() const {
         return capacity_;
