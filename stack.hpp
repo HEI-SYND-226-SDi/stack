2e2aff4615a4f015cf82164274d697ae22f6e342
Dominique Gabioud <dominique.gabioud@hevs.ch>
2018-02-20 16:09:49 +0100

    La vie est belle

--- stack.hpp
+++ stack.hpp
@@ -20,10 +20,10 @@ public:
         storage_[position_++] = element;
     }
 
-    void push(T&& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
-        storage_[position_++] = std::move(element);
-    }
+//    void push(T&& element) {
+//        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+//        storage_[position_++] = std::move(element);
+//    }
 
     T pop() {
         if (position_ < 0) throw std::out_of_range("Stack is empty");
