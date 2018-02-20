0b998830f6cf8ba8f957cb21728727a88459465b
DaniBri <daniel.briguet@students.hevs.ch>
2018-02-20 16:08:04 +0100

    First Fix, need to decrement befor acessing stack else it will acces next position

--- stack.hpp
+++ stack.hpp
@@ -5,30 +5,37 @@
 template <typename T>
 class Stack {
 public:
-    Stack(std::size_t capacity): storage_(new T[capacity]), capacity_(capacity) {}
+	Stack(std::size_t capacity): storage_(new T[capacity]), capacity_(capacity) {
 
+	}
+
+	// returns capacitiy of stack
     inline std::size_t capacity() const {
         return capacity_;
     }
 
+	// returns position in stack
     inline std::size_t size() const {
         return position_;
     }
 
+	// Adding object t stack
     void push(const T& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+		if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
         storage_[position_++] = element;
     }
 
+	// Adding object t stack
     void push(T&& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+		if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
         storage_[position_++] = std::move(element);
     }
 
+	// Removing object from stack
     T pop() {
-        if (position_ < 0) throw std::out_of_range("Stack is empty");
-        return std::move(storage_[position_--]);
-    }
+		if (position_ < 0) throw std::out_of_range("Stack is empty");
+		return std::move(storage_[--position_]);						// First Fix position is incremented after having been wirtten in
+	}
 
 private:
     T* storage_;
