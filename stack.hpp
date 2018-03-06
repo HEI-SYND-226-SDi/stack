a19860ef8d6e1731df3ce2f2923cdb2d9d465623
Loïc Gillioz <loic.gillioz@outlook.com>
2018-03-06 19:51:00 +0100

    corrected destructor

--- stack.hpp
+++ stack.hpp
@@ -7,7 +7,8 @@ class Stack {
 public:
     Stack(std::size_t capacity): storage_(new T[capacity]), capacity_(capacity) {}
 	~Stack(){
-		delete storage_;
+        //Warning : must delete arrays with [] !
+        delete[] storage_;
 	}
     inline std::size_t capacity() const {
         return capacity_;
