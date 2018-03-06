dcf8acd75ee7e6f4cb4e0e439e79c1c0d9b2d26f
Joost Laros <joost.laros92@gmail.com>
2018-03-06 19:16:35 +0100

    Solved 2nd and 3rd bug

--- stack.hpp
+++ stack.hpp
@@ -16,13 +16,25 @@ public:
     }
 
     void push(const T& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+        if (position_ >= capacity_)
+        {
+            throw std::out_of_range("Not enough capacity");
+        }
+        else
+        {
         storage_[position_++] = element;
+        }
     }
 
     void push(T&& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
-        storage_[position_++] = std::move(element);
+        if (position_ >= capacity_)
+        {
+            throw std::out_of_range("Not enough capacity");
+        }
+        else
+        {
+            storage_[position_++] = std::move(element);
+        }
     }
 
     T pop() {
