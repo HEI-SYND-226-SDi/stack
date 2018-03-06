abf7cab576e82986bc86ec34622e3d3bebbd75f0
Xdieu <36667001+Xdieu@users.noreply.github.com>
2018-03-06 15:51:45 +0100

    Fixed destructor, capacity bug and pop function
    Here it's my solution

--- stack.hpp
+++ stack.hpp
@@ -6,7 +6,9 @@ template <typename T>
 class Stack {
 public:
     Stack(std::size_t capacity): storage_(new T[capacity]), capacity_(capacity) {}
-
+    ~Stack() {
+        delete[] storage_; 
+    }
     inline std::size_t capacity() const {
         return capacity_;
     }
@@ -16,17 +18,17 @@ public:
     }
 
     void push(const T& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+    if (position_ >= capacity_) throw std::out_of_range("Not enough capacity");
         storage_[position_++] = element;
     }
 
     void push(T&& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+        if (position_ >= capacity_) throw std::out_of_range("Not enough capacity");
         storage_[position_++] = std::move(element);
     }
 
     T pop() {
-        if (position_ < 0) throw std::out_of_range("Stack is empty");
+        if (position_ <= 0) throw std::out_of_range("Stack is empty");
         return std::move(storage_[position_--]);
     }
 
