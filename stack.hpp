fe6df515e9f9e5abf9b1bd08ea2200ec578d0144
Damien Morend <damien.morend@students.hevs.ch>
2018-09-19 10:27:31 +0200

    bugfix2

--- stack.hpp
+++ stack.hpp
@@ -16,13 +16,13 @@ public:
     }
 
     void push(const T& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
-        //*(storage_ + ++position_) = element;
-        storage_[position_++] = element;
+        if (position_ >= capacity_) throw std::out_of_range("Not enough capacity");
+        *(storage_ + ++position_) = element;
+        //storage_[position_++] = element;
     }
 
     void push(T&& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+        if (position_ >= capacity_) throw std::out_of_range("Not enough capacity");
         *(storage_ + ++position_) = std::move(element);
        // storage_[position_++] = std::move(element);
     }
