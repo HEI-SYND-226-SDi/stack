fa7f1ebecf7d4d960758894c8e211246e099b834
Michaël Favre <michael.favre@students.hevs.ch>
2018-09-19 09:14:05 +0200

    bugfix pop

--- stack.hpp
+++ stack.hpp
@@ -17,17 +17,19 @@ public:
 
     void push(const T& element) {
         if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
-        storage_[position_++] = element;
+        storage_[position_] = element;
+        position_++;
     }
 
     void push(T&& element) {
         if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
-        storage_[position_++] = std::move(element);
+        storage_[position_] = std::move(element);
+        position_++;
     }
 
     T pop() {
         if (position_ < 0) throw std::out_of_range("Stack is empty");
-        return std::move(storage_[position_--]);
+        return std::move(storage_[--position_]);
     }
 
 private:
