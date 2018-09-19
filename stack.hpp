8e56576c6d4dbc700a631304eb2711885a695a37
Deadecho95 <43399859+Deadecho95@users.noreply.github.com>
2018-09-19 10:21:28 +0200

    debug move

--- stack.hpp
+++ stack.hpp
@@ -4,8 +4,18 @@
 
 template <typename T>
 class Stack {
+
+private:
+    T* storage_;
+    std::size_t capacity_;
+    std::size_t position_ = 0;
+
 public:
-    Stack(std::size_t capacity): storage_(new T[capacity]), capacity_(capacity) {}
+    Stack(std::size_t capacity): capacity_(capacity)
+    {
+
+        storage_ = new T[capacity_];
+    }
 
     inline std::size_t capacity() const {
         return capacity_;
@@ -22,16 +32,12 @@ public:
 
     void push(T&& element) {
         if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
-        storage_[position_++] = std::move(element);
+
+        *(storage_ + ++position_) = std::move(element);
     }
 
     T pop() {
         if (position_ < 0) throw std::out_of_range("Stack is empty");
         return std::move(storage_[position_--]);
     }
-
-private:
-    T* storage_;
-    std::size_t capacity_;
-    std::size_t position_ = 0;
 };
