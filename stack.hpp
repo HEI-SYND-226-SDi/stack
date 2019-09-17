0df8f9e006f93146c941cd92a3a379fbc69c33cb
devantheryl <devantheryl@windowslive.com>
2019-09-17 09:54:02 +0200

    Update stack.hpp
    fix second bug

--- stack.hpp
+++ stack.hpp
@@ -21,7 +21,7 @@ public:
     }
 
     void push(const T& element) {
-        if (position_ > capacity_) throw std::out_of_range("Not enough capacity");
+        if (position_ >= capacity_) throw std::out_of_range("Not enough capacity");
         storage_[position_++] = element;
     }
 
